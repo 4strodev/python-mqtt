4f493f05966e7f34c84d54f5b84576eea8ebc133
4strodev <jumarurex@gmail.com>
2023-04-19 17:24:53 +0200

    initial commit

---
+++ src/publisher.py
@@ -0,0 +1,26 @@
+import ssl
+import sys
+
+import paho.mqtt.client as paho
+broker = "broker.mqttdashboard.com"
+port = 1883
+# Crea la funció per callback. La funció que s'executa quan es produeix l'event on_publish
+
+
+def on_publish(client, userdata, result):
+    print("data published \n")
+    pass
+
+
+# Crea un objecte client
+client1 = paho.Client("control1")
+# Assigna la funció callback
+client1.on_publish = on_publish
+# Estableix la connexió
+client1.connect(broker, port)
+# Fa una publicacio
+ret = client1.publish("inspla/casa/llum/1", "on")
+if ret[0] == 0:
+    print("El topic s'ha enviat correctament!")
+else:
+    print("S'ha produït un error a l'hora d'enviar el topic")
---
+++ src/subscriber.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client
+import ssl
+import sys
+broker = "broker.mqttdashboard.com"
+port = 1883
+topic = 'inspla/casa/llum/1'
+
+
+# Crea la funció per callback. La funció que s'executa quan es produeix l'event de client connectat
+
+def on_connect(client, userdata, flags, rc):
+    print('connected (%s)' % client._client_id)
+    # Subscriu a un topic
+    client.subscribe(topic=topic, qos=2)
+
+# Crea la funció per callback. La funció que s'executa quan es produeix l'event on_message. Ha arribat un missatges des del broker
+
+
+def on_message(client, userdata, message):
+    print('------------------------------')
+    print('topic: %s' % message.topic)
+    print('payload: %s' % message.payload)
+    print('qos: %d' % message.qos)
+
+# Defineix una funció d'inicialització
+
+
+def main():
+    client = paho.mqtt.client.Client(
+        client_id='insplasll_1', clean_session=False)
+    # Assigna les funcions callback
+    client.on_connect = on_connect
+    client.on_message = on_message
+    # Es connecta al broker
+    client.connect(host=broker, port=port)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
+
+sys.exit(0)
