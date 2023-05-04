8e88b4d20ea325183016f3fb9b57136dee674ff7
4strodev <jumarurex@gmail.com>
2023-05-04 17:01:34 +0200

    idk

--- src/test/sensors.py
+++ src/test/sensors.py
@@ -1,12 +1,27 @@
 config = {
     "Menjador": {
         "Llum": 3,
-        "Temperatura": 2,
-        "Persiana": 1
+        "Temperatura": 1,
+        "Persiana": 2
     },
     "Cuina": {
-        "Llum": 3,
-        "Temperatura": 2,
+        "Llum": 2,
+        "Temperatura": 1,
         "Persiana": 1
-    }
+    },
+    "Labavo": {
+        "Llum": 2,
+        "Temperatura": 1,
+        "Persiana": 1
+    },
+    "Habitacio_1": {
+        "Llum": 1,
+        "Temperatura": 1,
+        "Persiana": 1
+    },
+    "Habitacio_2": {
+        "Llum": 1,
+        "Temperatura": 1,
+        "Persiana": 0
+    },
 }
