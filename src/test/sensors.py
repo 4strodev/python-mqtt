7c688ea5eb0b25e5fffc79a895fdf84a23342f3f
4strodev <jumarurex@gmail.com>
2023-05-04 16:56:02 +0200

    feat:sensors config

---
+++ src/test/sensors.py
@@ -0,0 +1,12 @@
+config = {
+    "Menjador": {
+        "Llum": 3,
+        "Temperatura": 2,
+        "Persiana": 1
+    },
+    "Cuina": {
+        "Llum": 3,
+        "Temperatura": 2,
+        "Persiana": 1
+    }
+}
